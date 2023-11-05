c7e9125b178bc9ba9f969064f6d118b6d3fa565e
RohitRothore <rohitrathorebanda123@gmail.com>
2023-11-06 02:28:39 +0530

    cors policy

--- SERVER/index.js
+++ SERVER/index.js
@@ -38,6 +38,24 @@ app.use(
 cloudinaryConnect();
 
 //routes
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    
+    // Pass to next layer of middleware
+    next();
+    });
 app.use("/api/v1/auth", userRoutes);
 app.use("/api/v1/profile", profileRoutes);
 app.use("/api/v1/course", courseRoutes);
